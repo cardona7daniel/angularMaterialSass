de5af1467c40f97a672a53c6ff22a1399af51a24
Luis cardona <luis.cardona@almundo.com>
2018-09-01 22:50:36 -0500

    Se agrega configuracion de sass y angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-doc';
+  title = 'app';
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatIconModule,
+  MatDividerModule,
+  MatCardModule,
+  MatTabsModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+
+const MaterialModules = [MatInputModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatIconModule,
+  MatCardModule,
+  MatDividerModule,
+  MatTabsModule];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterialModules,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
